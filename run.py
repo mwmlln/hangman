57e791d03883f36ba6c4428fae135b8837f9c33e
mwmlln <mwmlln@gmail.com>
2021-07-14 13:53:49 +0000

    ADD greeting message function

--- run.py
+++ run.py
@@ -1,17 +1,27 @@
 # python code goes here
+# list for question word
+
+animals = ["kangaroo", "chimpanzee", "tasmanian devil", "elephant", "giraffe", "wombat",]
+sea_creatures = ["jellyfish", "sea monkey", "monkfish", "seahorse", "dolphin", "octopus"]
+fruits = ["papaya", "dragon fruit", "kiwi fruit", "water melon", "lychee", "pineapple"]
 
 
+def greeting():
+    """
+    prompt user name input and display greetings
+    """
+    user_name = input("Enter your name:")
+    
+    print(f"Welcome to hangman game, {user_name}")
 
 
-
-
-main()
-    # greeting function
+def main():
+    greeting() # greeting function
     # choose category function
     # display question function
         #request user input
     # checking input function(While game_on = True)
-        # Check data type (try: except errortype:)
+        # Check data type -convert to lower case > (try: except errortype:)
         # check if answer is correct
             # if correct
                 # replace __ to the letter 
@@ -24,3 +34,5 @@ main()
                     # if complete (game_on = False) and user lose message and go to game over
                     # else go back to the top of the while loop (input)
     # game over 
+
+main()
